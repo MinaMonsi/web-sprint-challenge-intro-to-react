c3a8d6f5bef28fb1e47c413f6ed77e36cf33ab60
Marina Monsivais <minamonsi@me.com>
2021-03-06 14:10:56 -0600

    Trying to add a component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import './App.css';
+import Character from './components/Character';
 
 const App = () => {
   const [ data, setData] = useState({});
@@ -13,17 +14,22 @@ const App = () => {
     })
     .catch(err => console.log(err));
   }, []); 
-  
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  // you need to map over the array and use data.length to get to the information you want to access
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {/* {data.length > 0 ? data.map ((info) => <h2>{info.name}</h2>) : console.log(0)} */}
+      
+      <Character data={data}/> 
+
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,10 @@
 // Write your Character component here
+import React from "react";
+
+export default function Character (props){
+     return (
+         <div>{props.data.length > 0 ? data.map ((info) => 
+            <h2>{info.name}</h2>) : console.log(0)}</div>
+     
+     ); 
+}
