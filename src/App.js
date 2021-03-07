2014e42301ca0a8c50b370360f249b0dcc944218
Marina Monsivais <minamonsi@me.com>
2021-03-06 21:36:36 -0600

    Made changes to styling & cleaned up my code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import './App.css';
-import Character from './components/Character';
+import "./App.css";
+import Character from "./components/Character";
 
-const cards = document.createElement
 
 const App = () => {
   const [ data, setData] = useState([]);
   useEffect(() => {
     axios
-    .get('https://swapi.dev/api/people')
+    .get("https://swapi.dev/api/people")
     .then((res) => {
       console.log("axios response;", res.data)
       setData(res.data)
@@ -18,17 +17,9 @@ const App = () => {
   }, []); 
 
 
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-  
-
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
 
       <div>
         {data.map((item) => {
@@ -46,3 +37,10 @@ const App = () => {
 }
 
 export default App;
+
+// Try to think through what state you'll need for this app before starting. Then build out
+  // the state properties here.
+
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // side effect in a component, you want to think about which state and/or props it should
+  // sync up with, if any.
--- src/components/Character.js
+++ src/components/Character.js
@@ -3,23 +3,28 @@ import React from "react";
 import styled from "styled-components";
 
 const CardDiv = styled.div`
-color: #FFE5B4;
-font-weight: bold;
+color: #800000;
+text-align: center;
 max-width: 100%;
-border: 2px solid;
-border-color: #C04000;
-border-style: groove;
-margin: 20px 150px;
-padding: 10px;
+border: thick double;
+border-color: #D27D2D;
+margin: 30px 300px;
 `;
+
+const NameH2 = styled.h1`
+text-align: left;
+color: #4A0404;
+margin: 20px 80px;
+`;
+
 export default function Character (props){
      return (
          <CardDiv>
-         <h2>Name: {props.name}</h2>
-         <p>Eye Color: {props.eyeColor}</p>
-         <p>Hair Color: {props.hairColor}</p>
-         <p>Skin Color: {props.skinColor}</p>
-         <p>Birth Year: {props.birthYear}</p>
+         <NameH2>Name: {props.name}</NameH2>
+         <h2>Eye Color: {props.eyeColor}</h2>
+         <h2>Hair Color: {props.hairColor}</h2>
+         <h2>Skin Color: {props.skinColor}</h2>
+         <h2>Birth Year: {props.birthYear}</h2>
          </CardDiv>
      ); 
 }
